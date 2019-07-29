cc070cd83808719c713bf35c6fbe18bec59cdbdd
Ulyana Shornikova <ushornikova@brainstorminc.com>
2019-07-29 20:30:06 +0300

    Add project files.

---
+++ arrayexample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheClassWithOneParam[] var = new TheClassWithOneParam[5];
+            for (int i=0; i< var.Length; i++)
+            {
+                var[i] = new TheClassWithOneParam(i+1);
+                //TheClassWithOneParam whathever = var[i];
+                Console.WriteLine(var[i].TheElement);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class TheClassWithOneParam
+    {
+        public int TheElement { get; set; }
+
+        public TheClassWithOneParam(int theelement)
+        {
+            TheElement = theelement;
+        }
+
+    }
+}
